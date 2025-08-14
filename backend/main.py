d215d1102ca3d8c4db56cc275a57432821ca9c4e
Daxigi <maximilianogimenez91@gmail.com>
2025-08-14 09:56:47 -0300

    Revert "fix in main"
    This reverts commit 3cc0de3556295d8385edfaa44cc9625c7b9e2279.

--- backend/main.py
+++ backend/main.py
@@ -78,7 +78,7 @@ def clean_non_json_values(data):
     if isinstance(data, dict):
         return {k:clean_non_json_values(v) for k, v in data.items()}
     if isinstance(data, list):
-        return [clean_non_json_values(i) for i in data]
+        return [vlean_non_json_values(i) for i in data]
     if isinstance(data, float) and (np.isnan(data) or np.isinf(data)):
         return None
     return data
